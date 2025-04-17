ebebc405869eeb4fe8fc25e586f87f4434981b02
Luis Fernando Botero <luis.botero2001@gmail.com>
2025-04-16 21:13:07 -0500

    metadata included in feature names

--- flairReformatting.py
+++ flairReformatting.py
@@ -1,3 +1,4 @@
+import ast
 import os
 import numpy as np
 import pandas as pd
@@ -17,30 +18,37 @@ inter_dir=args.dataDir+'/processedData'
 
 # ---------------------------- UTILITIES ----------------------------
 
-def tupleExtraction(df, tuplas):
-    tuple_df=pd.DataFrame()
-    for col in tuplas: 
-        # Extract tuple values and clean them
-        vals = [re.sub(r'[()]', '', str(x)) for x in df[col]]  # Convert to strings
-        vals = [x.split(',') for x in vals]  
+def tupleExtraction(df):
+    for col in df.columns:
+        try: 
+            parsed = ast.literal_eval(df[col][0])  # [0] because its a dataframe with 1 row
+            if isinstance(parsed, tuple):
+                # Extract tuple values and clean them
+                vals = [re.sub(r'[()]', '', str(x)) for x in df[col]]  # Convert to strings
+                vals = [x.split(',') for x in vals]  
 
-        # Create new DataFrame with the extracted values
-        asciiNum = 97  # ASCII 'a'
-        temp_df = pd.DataFrame()
+                # Create new DataFrame with the extracted values
+                asciiNum = 97  # ASCII 'a'
+                temp_df = pd.DataFrame()
 
-        for row_idx, tpl in enumerate(vals):  
-            for i in range(len(tpl)):
-                newAscii = chr(asciiNum + i)  # Convert to letter
-                newCol = f"{col}.{newAscii}"  
+                for row_idx, tpl in enumerate(vals):  
+                    for i in range(len(tpl)):
+                        newAscii = chr(asciiNum + i)  # Convert to letter
+                        newCol = f"{newAscii}.{col}"  
+                        
+                        if newCol not in temp_df.columns:
+                            temp_df[newCol] = None  
+                        
+                        temp_df.loc[row_idx, newCol] = tpl[i]
+                        temp_df[newCol]=pd.to_numeric(temp_df[newCol])
                 
-                if newCol not in temp_df.columns:
-                    temp_df[newCol] = None  
-                
-                temp_df.loc[row_idx, newCol] = tpl[i]
-                temp_df[newCol]=pd.to_numeric(temp_df[newCol])
-                
-        tuple_df = pd.concat([tuple_df, temp_df], axis=1)
-    return tuple_df
+                df.drop(columns=[col], inplace=True)  
+                df = pd.concat([df, temp_df], axis=1)
+        except (ValueError, SyntaxError):
+            
+            pass
+
+    return df
 
 
 def reformating(input_file):
@@ -52,11 +60,16 @@ def reformating(input_file):
 
     df = pd.read_csv(input_file)
 
+    df['Feature Name']=df['Feature Name']+"/"+df['Feature Class']+"/"+df['Image type']
+    print(df.shape)
     # only considering columns 'Feature Name' and 'Segment...' as columns and values respectively
     df = df.set_index('Feature Name')
     df.drop(columns=['Image type', 'Feature Class'], inplace=True)
 
     df_transposed = df.T 
+    
+    id_col=[col for col in df_transposed.columns if col.startswith("Id/")][0]
+    df_transposed.rename(columns={id_col: "Id"}, inplace=True)
 
     output_file = inter_dir+'/'+file_name
 
@@ -79,15 +92,8 @@ for file in files:
         reformating(args.dataDir+'/'+file) 
         df = pd.read_csv(inter_dir+'/'+file) #reads processed file
         
-        tuplas = ['Spacing', 'Size', 'Spacing.1', 'Size.1', 'BoundingBox', 'CenterOfMassIndex', 'CenterOfMass']
-        df_tuple=tupleExtraction(df, tuplas)
-
-        df_tuple['Id']=df['Id']
-        df.set_index('Id', inplace=True)
-        df_tuple.set_index('Id', inplace=True)
-
-        df=pd.merge(df_tuple, df.drop(columns=tuplas),  on='Id', how='inner')
-
+        df=tupleExtraction(df)
+        
         # Setting up labels based for tumor grade (high or low) implied by the file name 
         match = re.search(r'\d', file)
         first_digit = match.group() # There is no error handlidng because if there is no match, file name is not valid.
